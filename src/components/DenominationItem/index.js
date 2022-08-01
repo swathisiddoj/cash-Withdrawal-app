3816f27d82f8ea2a165982cc59009ca034679d67
swathisiddoj <swathisiddoj10@gmail.com>
2022-08-01 15:08:16 +0000

    cashWithdrawalApp

---
+++ src/components/DenominationItem/index.js
@@ -0,0 +1,23 @@
+import './index.css'
+
+const DenominationItem = props => {
+  const {denominationDetails, updateBalance} = props
+  const {value} = denominationDetails
+
+  const onClickDenomination = () => {
+    updateBalance(value)
+  }
+  return (
+    <li className="denomination-item">
+      <button
+        className="denomination-button"
+        type="button"
+        onClick={onClickDenomination}
+      >
+        {value}
+      </button>
+    </li>
+  )
+}
+
+export default DenominationItem
